24f9ccda6723a854777b7fcc1f0726aaa10c6714
Damian Kokot <73639471+damiankokot45@users.noreply.github.com>
2023-05-24 19:12:31 +0200

    update

---
+++ PythonMath/25.04/math.py
@@ -0,0 +1,9 @@
+print(2**3)
+print(9%3)
+print(8**(1/3))
+
+liczba=int(input("Podaj liczbe"))
+print(liczba+1)
+
+liczbazm=float(input("Podaj liczbe"))
+print(liczbazm+1)
--- python.py
+++ python.py
@@ -1,5 +1,25 @@
-print("test czy dziala")
+print("Pierwsze działanie matematyczne dodawanie")
 
 a=10
 b=20
-print(a+b)
+wynik=a+b
+print(wynik)
+
+print("Drugie działanie matematyczne odejmowanie")
+
+odejmowanieliczba=5
+odejmowanieliczba1=2
+odejmowaniewynik=odejmowanieliczba-odejmowanieliczba1
+print(odejmowaniewynik)
+
+print("Trzecie działanie matematyczne mnożenie")
+
+mnozenieliczba1=2
+mnozenieliczba2=5
+print(mnozenieliczba1*mnozenieliczba2)
+
+print("Czwarte dziłanie matematyczne dzielenie")
+
+dzielenieliczba=10
+dzielenieliczba1=4
+print(dzielenieliczba/dzielenieliczba1)
